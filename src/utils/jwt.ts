863ee8d537ef9dd33cf7c037c32b3e79366be16a
Nadhir Ferdji <nadhirxz@gmail.com>
2023-10-06 11:36:55 +0100

    Add jwt

---
+++ src/utils/jwt.ts
@@ -0,0 +1,6 @@
+import jwt from 'jsonwebtoken';
+
+const jwtExpire = 3 * 24 * 60 * 60;
+
+export const generateToken = (id: string) => jwt.sign({ id }, process.env.JWT_SECRET!, { expiresIn: jwtExpire });
+export const verifyToken = (token: string) => jwt.verify(token, process.env.JWT_SECRET!);
