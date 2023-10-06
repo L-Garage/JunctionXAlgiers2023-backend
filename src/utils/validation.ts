433e3e148edbfdf5471c59528e082bbb659bb178
Nadhir Ferdji <nadhirxz@gmail.com>
2023-10-06 14:06:54 +0100

    Fix validate request schema

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -3,11 +3,7 @@ import { z, AnyZodObject } from 'zod';
 
 export const validateRequestSchema = (schema: AnyZodObject) => async (req: Request, res: Response, next: NextFunction) => {
 	try {
-		await schema.parseAsync({
-			body: req.body,
-			query: req.query,
-			params: req.params,
-		});
+		await schema.parseAsync(req.body);
 
 		return next();
 	} catch (error) {
@@ -16,16 +12,12 @@ export const validateRequestSchema = (schema: AnyZodObject) => async (req: Reque
 };
 
 export const registerSchema = z.object({
-	body: z.object({
-		email: z.string().email('Invalid email address'),
-		name: z.string().min(1, 'Name is required'),
-		password: z.string().min(8, 'Password must be at least 8 characters'),
-	}),
+	email: z.string().email('Invalid email address'),
+	name: z.string().min(1, 'Name is required'),
+	password: z.string().min(8, 'Password must be at least 8 characters'),
 });
 
 export const loginSchema = z.object({
-	body: z.object({
-		email: z.string().email('Invalid email address'),
-		password: z.string().min(8, 'Password must be at least 8 characters'),
-	}),
+	email: z.string().email('Invalid email address'),
+	password: z.string().min(8, 'Password must be at least 8 characters'),
 });
